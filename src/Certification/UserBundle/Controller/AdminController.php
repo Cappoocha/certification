cc03602ed5ad61b9124c3e3717bf255481aff8d5
cappuccino <katerinacappuccino@gmail.com>
2015-03-30 23:06:34 +0400

    Create admin controller

---
+++ src/Certification/UserBundle/Controller/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author Катерина
+ * Date: 30.03.15
+ * Time: 15:25
+ */
+
+namespace Certification\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class AdminController
+ * @package Certification\UserBundle\Controller
+ */
+class AdminController extends Controller
+{
+
+} 
